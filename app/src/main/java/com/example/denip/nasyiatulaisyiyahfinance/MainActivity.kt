49659a8ffb729f8609acc0948c63c885b3093b01
deniprayoga <deniprayogaa@gmail.com>
2017-08-13 14:45:02 +0700

    improve logo on login layout reformat code

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/MainActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.denip.nasyiatulaisyiyahfinance
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var text: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text = editText.text.toString()
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
 
         //get firebase auth instance
         val auth = FirebaseAuth.getInstance()
@@ -21,9 +26,9 @@ class MainActivity : AppCompatActivity() {
 
         val authListener = FirebaseAuth.AuthStateListener {
             firebaseAuth ->
-            val user:FirebaseUser? = firebaseAuth.currentUser
+            val user: FirebaseUser? = firebaseAuth.currentUser
 
-            if (user==null){
+            if (user == null) {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
