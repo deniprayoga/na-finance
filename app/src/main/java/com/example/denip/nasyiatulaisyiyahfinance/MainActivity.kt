3884d3a69840d33839aedfc2f3b854a733663fe9
deniprayoga <deniprayogaa@gmail.com>
2017-08-17 09:32:05 +0700

    - add dependencies in gradle - add expense fragment - add income fragment - add icons

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/MainActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/MainActivity.kt
@@ -2,27 +2,32 @@ package com.example.denip.nasyiatulaisyiyahfinance
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var text: String
+    private var sectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    private var mViewPager: ViewPager? = null
+
     val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        text = editText.text.toString()
-        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
-
-        //get current user
-        //val currentUser = FirebaseAuth.getInstance().currentUser
 
         val authListener = FirebaseAuth.AuthStateListener {
             firebaseAuth ->
@@ -32,6 +37,18 @@ class MainActivity : AppCompatActivity() {
                 launchLoginActivity()
             }
         }
+
+//        val toolbar = findViewById(R.id.toolbar) as Toolbar
+//        setSupportActionBar(toolbar)
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = findViewById(R.id.container) as ViewPager
+        mViewPager!!.adapter = sectionsPagerAdapter
+
+        mViewPager!!.currentItem = 1
 
         sign_out_button.setOnClickListener {
             val dialogBuilder = AlertDialog.Builder(this)
@@ -48,10 +65,97 @@ class MainActivity : AppCompatActivity() {
         }
 
         auth.addAuthStateListener(authListener)
+
     }
 
     private fun launchLoginActivity() {
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * A [FragmentPagerAdapter] that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return PlaceholderFragment.newInstance(position + 1)
+        }
+
+        override fun getCount(): Int {
+            // Show 3 total pages.
+            return 3
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            when (position) {
+                0 -> return "SECTION 1"
+                1 -> return "SECTION 2"
+                2 -> return "SECTION 3"
+            }
+            return null
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    class PlaceholderFragment : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+
+            if (arguments.getInt(ARG_SECTION_NUMBER) == 1) {
+                val rootView = inflater!!.inflate(R.layout.fragment_income, container, false)
+                return rootView
+            } else if (arguments.getInt(ARG_SECTION_NUMBER) == 2) {
+                val rootView = inflater!!.inflate(R.layout.fragment_main, container, false)
+                return rootView
+            } else {
+                val rootView = inflater!!.inflate(R.layout.fragment_expense, container, false)
+                return rootView
+            }
+        }
+
+        companion object {
+            /**
+             * The fragment argument representing the section number for this
+             * fragment.
+             */
+            private val ARG_SECTION_NUMBER = "section_number"
+
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            fun newInstance(sectionNumber: Int): PlaceholderFragment {
+                val fragment = PlaceholderFragment()
+                val args = Bundle()
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+                fragment.arguments = args
+                return fragment
+            }
+        }
     }
 }
