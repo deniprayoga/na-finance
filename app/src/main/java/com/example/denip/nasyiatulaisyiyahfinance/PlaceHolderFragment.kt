c0a8c5b1feea0cd4f4657d2b21a8faf8ee03a450
deniprayoga <deniprayogaa@gmail.com>
2017-08-18 16:46:57 +0700

    - improve code structure on PlaceHolderFragment class - improve UI for fragment expense, income, and main - reformat code

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/PlaceHolderFragment.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/PlaceHolderFragment.kt
@@ -1,8 +1,8 @@
 package com.example.denip.nasyiatulaisyiyahfinance
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
@@ -18,13 +18,6 @@ class PlaceholderFragment : Fragment(), View.OnClickListener {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
-
-
-    override fun onClick(v: View?) {
-
-
-    }
-
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
@@ -34,27 +27,45 @@ class PlaceholderFragment : Fragment(), View.OnClickListener {
         } else if (arguments.getInt(ARG_SECTION_NUMBER) == 2) {
             val fragmentMainView = inflater!!.inflate(R.layout.fragment_main, container, false)
             fragmentMainView?.button2_sign_out?.setOnClickListener {
-                val dialogBuilder = AlertDialog.Builder(context)
-                dialogBuilder
-                    .setTitle(getString(R.string.confirmation))
-                    .setMessage(getString(R.string.are_you_sure))
-                    .setPositiveButton(getString(R.string.yes), { _, _ ->
-                        auth.signOut()
-                    })
-                    .setNegativeButton(getString(R.string.no), { dialog, _ ->
-                        dialog.cancel()
-                    })
-                    .show()
+                showDialog()
             }
 
-            fragmentMainView.main_expense_button.setOnClickListener {
-
-            }
             return fragmentMainView
         } else {
             val fragmentExpenseView = inflater!!.inflate(R.layout.fragment_income, container, false)
             return fragmentExpenseView
         }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.main_expense_button -> {
+                val fragmentExpense: Fragment = ExpenseFragment()
+                replaceFragment(fragmentExpense)
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction
+            .replace(R.id.container_fragment_main, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun showDialog() {
+        val dialogBuilder = AlertDialog.Builder(context)
+        dialogBuilder
+            .setTitle(getString(R.string.confirmation))
+            .setMessage(getString(R.string.are_you_sure))
+            .setPositiveButton(getString(R.string.yes), { _, _ ->
+                auth.signOut()
+            })
+            .setNegativeButton(getString(R.string.no), { dialog, _ ->
+                dialog.cancel()
+            })
+            .show()
     }
 
     companion object {
