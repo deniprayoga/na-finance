086165e94c43cdd4329d10bb2a5f4f3abed64c17
deniprayoga <deniprayogaa@gmail.com>
2017-08-27 16:35:52 +0700

    - convert android test file to kotlin - able to pick image from gallery but yet to capture photo with camera - add xml file for income and expense layout - update layout on add expense - change background on fragment expense - change background on income expense - add uses permission in manifest (camera and write external storage) - add tedbottompicker and tedpermission to gradle - add jcenter() and maven jitpack.io to gradle - reformat

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/AddExpenseActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/AddExpenseActivity.kt
@@ -1,26 +1,36 @@
 package com.example.denip.nasyiatulaisyiyahfinance
 
 import android.content.Context
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import gun0912.tedbottompicker.TedBottomPicker
 import kotlinx.android.synthetic.main.activity_add_expense.*
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
+import java.util.ArrayList
 
 class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
     CalendarDatePickerDialogFragment.OnDateSetListener {
-
     companion object {
         val FRAG_TAG_DATE_PICKER = "fragment_date_picker_name"
         val context: Context? = null
+        var selectedUri: Uri? = null
+        lateinit var glideRequestManager: RequestManager
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_expense)
+        glideRequestManager = Glide.with(this)
 
         expense_value_field.text
 
@@ -39,6 +49,7 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
         clear_button.setOnClickListener(this)
         check_value_button.setOnClickListener(this)
         calendar_button.setOnClickListener(this)
+        pick_image_button.setOnClickListener(this)
 
     }
 
@@ -93,7 +104,47 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
             R.id.calendar_button -> {
                 showCalendar()
             }
+            R.id.pick_image_button -> {
+                pickImage()
+            }
         }
+    }
+
+    private fun pickImage() {
+        val permissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                val bottomSheetDialogFragment = TedBottomPicker.Builder(this@AddExpenseActivity)
+                    .setOnImageSelectedListener { uri ->
+                        Log.d("ted", "uri:" + uri)
+                        Log.d("ted", "uri.path:" + uri.path)
+                        selectedUri = uri
+
+                        show_image_imageview.visibility = View.VISIBLE
+                        selected_photos_container.visibility = View.VISIBLE
+                        show_image_imageview.post {
+                            glideRequestManager
+                                .load(uri)
+                                .into(show_image_imageview)
+                        }
+                    }
+                    .setSelectedUri(selectedUri)
+                    .setPeekHeight(1200)
+                    .create()
+                bottomSheetDialogFragment.show(supportFragmentManager)
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
+                Toast.makeText(this@AddExpenseActivity, "Permission denied\n" +
+                    deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        TedPermission(this@AddExpenseActivity)
+            .setPermissionListener(permissionListener)
+            .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+            .setPermissions("android.permission.WRITE_EXTERNAL_STORAGE")
+            .check()
     }
 
     private fun showCalendar() {
