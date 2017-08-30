651da3a712215ca912f75d068cf7b7a8f039d08f
deniprayoga <deniprayogaa@gmail.com>
2017-08-30 23:32:33 +0700

    add actionBar in addIncomeLayout

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/AddIncomeActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/AddIncomeActivity.kt
@@ -2,11 +2,37 @@ package com.example.denip.nasyiatulaisyiyahfinance
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 
 class AddIncomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_income)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById(R.id.toolbar_add_income_layout) as Toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_bar_done -> finish()
+            android.R.id.home -> onBackPressed()
+        }
+        return true
     }
 }
