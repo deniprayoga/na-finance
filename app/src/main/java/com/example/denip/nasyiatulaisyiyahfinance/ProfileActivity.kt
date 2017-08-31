555ac77be04704ac4e1765d808af12bf15906ece
deniprayoga <deniprayogaa@gmail.com>
2017-08-31 09:44:44 +0700

    - add profile activity - reformat

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/ProfileActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/ProfileActivity.kt
@@ -1,45 +1,29 @@
 package com.example.denip.nasyiatulaisyiyahfinance
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
-import android.view.View
-import kotlinx.android.synthetic.main.activity_setting.*
 
-class SettingActivity : AppCompatActivity(), View.OnClickListener {
+class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        setContentView(R.layout.activity_profile)
 
         initLayout()
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }*/
-
     private fun initLayout() {
         initToolbar()
-        profile_button.setOnClickListener(this)
     }
 
     private fun initToolbar() {
-        val toolbar = findViewById(R.id.toolbar_setting_layout) as Toolbar
+        val toolbar = findViewById(R.id.toolbar_profile_layout) as Toolbar
         setSupportActionBar(toolbar)
 
         supportActionBar!!.setHomeButtonEnabled(true)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.profile_button -> startActivity(Intent(this, ProfileActivity::class.java))
-        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
