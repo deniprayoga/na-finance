9b6bf3f1705a76c65e95b0277b95382b08a623d3
deniprayoga <deniprayogaa@gmail.com>
2017-09-04 13:56:50 +0700

    - add AddAmountActivity - navigate between AddExpenseActivity and AddAmountActivity (not fixed yet) - update background numpad button - add drawables - update add expense layout - add some strings, colors, and styles - update manifest - reformat

---
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/AddAmountActivity.kt
@@ -0,0 +1,127 @@
+package com.example.denip.nasyiatulaisyiyahfinance
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add_amount.*
+
+class AddAmountActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_amount)
+
+        initLayout()
+
+        val bundle: Bundle? = intent!!.extras
+        val amount: String? = bundle?.getString(getString(R.string.EXTRA_AMOUNT))
+        expense_amount_field_add_amount.setText(amount)
+    }
+
+    private fun initLayout() {
+        expense_amount_field_add_amount?.isFocusable = false
+        initToolbar()
+        initNumpad()
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById(R.id.toolbar_add_expense_layout) as Toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun initNumpad() {
+        one_button.setOnClickListener(this)
+        two_button.setOnClickListener(this)
+        three_button.setOnClickListener(this)
+        four_button.setOnClickListener(this)
+        five_button.setOnClickListener(this)
+        six_button.setOnClickListener(this)
+        seven_button.setOnClickListener(this)
+        eight_button.setOnClickListener(this)
+        nine_button.setOnClickListener(this)
+        zero_button.setOnClickListener(this)
+        clear_button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.zero_button -> {
+                addZero()
+            }
+            R.id.one_button -> {
+                addNumberToField(1)
+            }
+            R.id.two_button -> {
+                addNumberToField(2)
+            }
+            R.id.three_button -> {
+                addNumberToField(3)
+            }
+            R.id.four_button -> {
+                addNumberToField(4)
+            }
+            R.id.five_button -> {
+                addNumberToField(5)
+            }
+            R.id.six_button -> {
+                addNumberToField(6)
+            }
+            R.id.seven_button -> {
+                addNumberToField(7)
+            }
+            R.id.eight_button -> {
+                addNumberToField(8)
+            }
+            R.id.nine_button -> {
+                addNumberToField(9)
+            }
+            R.id.clear_button -> {
+                clearField()
+            }
+        }
+    }
+
+    private fun addNumberToField(num: Int?) {
+        val currentAmount = expense_amount_field_add_amount?.text.toString()
+        expense_amount_field_add_amount?.setText(currentAmount.plus(num))
+    }
+
+    private fun addZero() {
+        val currentAmount = expense_amount_field_add_amount?.text.toString()
+        if (currentAmount.isEmpty()) {
+            addEmptyString("")
+        } else {
+            addNumberToField(0)
+        }
+    }
+
+    private fun clearField() {
+        expense_amount_field_add_amount.text.clear()
+    }
+
+    private fun addEmptyString(s: String?) {
+        val currentAmount = expense_amount_field_add_amount?.text.toString()
+        expense_amount_field_add_amount?.setText(currentAmount.plus(s))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_bar_done -> finish()
+            android.R.id.home -> {
+                val currentAmount = expense_amount_field_add_amount?.text.toString()
+/*                val bundleAmount = Bundle()
+                bundleAmount.putString(getString(R.string.EXTRA_AMOUNT), currentAmount)*/
+                val intent = Intent(this, AddExpenseActivity::class.java)
+                intent.putExtra(getString(R.string.EXTRA_AMOUNT), currentAmount)
+                startActivity(intent)
+            }
+        }
+        return true
+    }
+}
--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/AddExpenseActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/AddExpenseActivity.kt
@@ -2,6 +2,7 @@ package com.example.denip.nasyiatulaisyiyahfinance
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -37,26 +38,20 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
         setContentView(R.layout.activity_add_expense)
         glideRequestManager = Glide.with(this)
 
+        val bundle: Bundle? = intent!!.extras
+        val amount: String? = bundle?.getString(getString(R.string.EXTRA_AMOUNT))
+        expense_amount_field.setText(amount)
         initLayout()
     }
 
     private fun initLayout() {
+        expense_amount_field.isCursorVisible = false
         initToolbar()
         showCurrentDate()
-        one_button.setOnClickListener(this)
-        two_button.setOnClickListener(this)
-        three_button.setOnClickListener(this)
-        four_button.setOnClickListener(this)
-        five_button.setOnClickListener(this)
-        six_button.setOnClickListener(this)
-        seven_button.setOnClickListener(this)
-        eight_button.setOnClickListener(this)
-        nine_button.setOnClickListener(this)
-        zero_button.setOnClickListener(this)
-        clear_button.setOnClickListener(this)
-        check_value_button.setOnClickListener(this)
         calendar_button.setOnClickListener(this)
         pick_image_button.setOnClickListener(this)
+        expense_amount_field.setOnClickListener(this)
+
     }
 
     private fun initToolbar() {
@@ -68,13 +63,15 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
     }
 
     override fun onDateSet(dialog: CalendarDatePickerDialogFragment?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
-        calendar_result_text.text = getString(R.string.calendar_date_picker_result_values, year, monthOfYear, dayOfMonth)
+        calendar_result_text.text = getString(R.string.calendar_date_picker_result_values, year, monthOfYear + 1, dayOfMonth)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
             R.id.action_bar_done -> finish()
-            android.R.id.home -> onBackPressed()
+            android.R.id.home -> {
+                finish()
+            }
         }
         return true
     }
@@ -86,45 +83,6 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.check_value_button -> {
-                val expenseValueField = expense_amount_field.text.toString()
-                Toast.makeText(this, expenseValueField, Toast.LENGTH_SHORT).show()
-            }
-            R.id.zero_button -> {
-                val currentValue = expense_amount_field.text.toString()
-                if (currentValue.isEmpty()) {
-                    avoidZeroInFirstInput("")
-                } else {
-                    addNumberToField(0)
-                }
-            }
-            R.id.one_button -> {
-                addNumberToField(1)
-            }
-            R.id.two_button -> {
-                addNumberToField(2)
-            }
-            R.id.three_button -> {
-                addNumberToField(3)
-            }
-            R.id.four_button -> {
-                addNumberToField(4)
-            }
-            R.id.five_button -> {
-                addNumberToField(5)
-            }
-            R.id.six_button -> {
-                addNumberToField(6)
-            }
-            R.id.seven_button -> {
-                addNumberToField(7)
-            }
-            R.id.eight_button -> {
-                addNumberToField(8)
-            }
-            R.id.nine_button -> {
-                addNumberToField(9)
-            }
             R.id.clear_button -> {
                 clearField()
             }
@@ -134,7 +92,17 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
             R.id.pick_image_button -> {
                 pickImage()
             }
+            R.id.expense_amount_field -> {
+                navigateToAddAmount()
+            }
         }
+    }
+
+    private fun navigateToAddAmount() {
+        val intent = Intent(this, AddAmountActivity::class.java)
+        val amount = expense_amount_field?.text.toString()
+        intent.putExtra(getString(R.string.EXTRA_AMOUNT), amount)
+        startActivity(intent)
     }
 
     private fun pickImage() {
@@ -146,15 +114,15 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
                         Log.d("ted", "uri.path:" + uri.path)
                         selectedUri = uri
 
-                        show_image_imageview.visibility = View.VISIBLE
-                        show_image_imageview.post {
+                        image_preview.visibility = View.VISIBLE
+                        image_preview.post {
                             glideRequestManager
                                 .load(uri)
                                 .fitCenter()
-                                .into(show_image_imageview)
+                                .into(image_preview)
                         }
 
-                        show_image_imageview.layoutParams = LinearLayout.LayoutParams(250, 250)
+                        image_preview.layoutParams = LinearLayout.LayoutParams(800, 800)
                     }
 
                     .setSelectedUri(selectedUri)
@@ -194,13 +162,4 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
         expense_amount_field.text.clear()
     }
 
-    private fun addNumberToField(num: Int?) {
-        val expenseValueField = expense_amount_field?.text.toString()
-        expense_amount_field?.setText(expenseValueField.plus(num))
-    }
-
-    private fun avoidZeroInFirstInput(s: String?) {
-        val expenseValueField = expense_amount_field?.text.toString()
-        expense_amount_field?.setText(expenseValueField.plus(s))
-    }
 }
