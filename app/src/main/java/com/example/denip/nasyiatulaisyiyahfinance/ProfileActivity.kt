43be010cdcd48565884881674b051a9817562ef6
deniprayoga <deniprayogaa@gmail.com>
2017-09-08 13:35:53 +0700

    restructure code

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/ProfileActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/ProfileActivity.kt
@@ -1,16 +1,13 @@
 package com.example.denip.nasyiatulaisyiyahfinance
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity(), View.OnClickListener {
 
-    val context : Context? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -34,40 +31,34 @@ class ProfileActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.edit_profile_button -> {
-                profile_name_field.isEnabled = true
-                phone_number_profile_field.isEnabled = true
-                location_profile_field.isEnabled = true
-                edit_profile_button.visibility = View.INVISIBLE
-                save_profile_button.visibility = View.VISIBLE
+                enableFields()
             }
             R.id.save_profile_button -> {
-                profile_name_field.isEnabled = false
-                phone_number_profile_field.isEnabled = false
-                location_profile_field.isEnabled = false
-                edit_profile_button.visibility = View.VISIBLE
-                save_profile_button.visibility = View.INVISIBLE
+                disableFields()
             }
         }
     }
 
-    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        return super.onPrepareOptionsMenu(menu)
+    private fun enableFields() {
+        profile_name_field.isEnabled = true
+        phone_number_profile_field.isEnabled = true
+        location_profile_field.isEnabled = true
+        edit_profile_button.visibility = View.INVISIBLE
+        save_profile_button.visibility = View.VISIBLE
+    }
+
+    private fun disableFields() {
+        profile_name_field.isEnabled = false
+        phone_number_profile_field.isEnabled = false
+        location_profile_field.isEnabled = false
+        edit_profile_button.visibility = View.VISIBLE
+        save_profile_button.visibility = View.INVISIBLE
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
         when (id) {
             android.R.id.home -> onBackPressed()
-            R.id.action_bar_edit_profile -> {
-                profile_name_field.isEnabled = true
-                phone_number_profile_field.isEnabled = true
-                location_profile_field.isEnabled = true
-            }
-            R.id.action_bar_save_profile -> {
-                profile_name_field.isEnabled = false
-                phone_number_profile_field.isEnabled = false
-                location_profile_field.isEnabled = false
-            }
         }
 
         return super.onOptionsItemSelected(item)
