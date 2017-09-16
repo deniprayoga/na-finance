0717e6cc8478f2d5417eb538c72d2d52f2666fd1
deniprayoga <deniprayogaa@gmail.com>
2017-09-17 00:46:46 +0700

    add expandable list view when select category setting option

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/SettingActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/SettingActivity.kt
@@ -9,17 +9,24 @@ import android.view.View
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_setting.*
+import kotlin.collections.HashMap
 
 class SettingActivity : AppCompatActivity(), View.OnClickListener {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var listAdapter: ExpandableListAdapter? = null
+    private lateinit var listDataHeader: MutableList<String>
+    private lateinit var listDataChild: HashMap<String, List<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
         initLayout()
+        initAuth()
+    }
 
+    private fun initAuth() {
         val authListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
             val user: FirebaseUser? = firebaseAuth.currentUser
 
@@ -35,18 +42,64 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
         finish()
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }*/
-
     private fun initLayout() {
         initToolbar()
         setting_profile.setOnClickListener(this)
-        setting_category.setOnClickListener(this)
         setting_change_password.setOnClickListener(this)
         setting_sign_out.setOnClickListener(this)
+        initExpandableListView()
+    }
+
+    private fun initExpandableListView() {
+        prepareListDataCategory()
+        listAdapter = ExpandableListAdapter(this, listDataHeader, listDataChild)
+
+        lvExp?.setAdapter(listAdapter)
+
+        lvExp?.setOnGroupClickListener { parent, v, groupPosition, id ->
+            false
+        }
+
+        // Listview Group expanded listener
+        /*        lvExp?.setOnGroupExpandListener { groupPosition ->
+            Toast.makeText(applicationContext, listDataHeader[groupPosition] + " Expanded",
+                Toast.LENGTH_SHORT).show()
+        }*/
+
+        // Listview Group collasped listener
+        /*lvExp?.setOnGroupCollapseListener { groupPosition ->
+            Toast.makeText(applicationContext,
+                listDataHeader[groupPosition] + " Collapsed",
+                Toast.LENGTH_SHORT).show()
+        }*/
+
+        // Listview on child click listener
+        lvExp?.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+            when (childPosition) {
+                0 -> launchCategoryExpenseSetting()
+                1 -> launchCategoryIncomeSetting()
+            }
+            false
+            /*            Toast.makeText(
+                    applicationContext,
+                    listDataHeader[groupPosition]
+                        + " : "
+                        + listDataChild[listDataHeader[groupPosition]]!![childPosition], Toast.LENGTH_SHORT)
+                    .show()*/
+        }
+    }
+
+    private fun prepareListDataCategory() {
+        listDataHeader = ArrayList()
+        listDataChild = HashMap()
+
+        listDataHeader.add(getString(R.string.category_title))
+
+        val categories = ArrayList<String>()
+        categories.add(getString(R.string.expense))
+        categories.add(getString(R.string.income))
+
+        listDataChild.put(listDataHeader[0], categories)
     }
 
     private fun initToolbar() {
@@ -59,12 +112,19 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.setting_profile -> startActivity(Intent(this, ProfileActivity::class.java))
-            R.id.setting_category -> startActivity(Intent(this, CategorySettingActivity::class.java))
             R.id.setting_change_password -> startActivity(Intent(this, ChangePasswordActivity::class.java))
             R.id.setting_sign_out -> {
                 showDialogSignOut()
             }
         }
+    }
+
+    private fun launchCategoryIncomeSetting() {
+        startActivity(Intent(this, CategoryIncomeSettingActivity::class.java))
+    }
+
+    private fun launchCategoryExpenseSetting() {
+        startActivity(Intent(this, CategoryExpenseSettingActivity::class.java))
     }
 
     private fun showDialogSignOut() {
