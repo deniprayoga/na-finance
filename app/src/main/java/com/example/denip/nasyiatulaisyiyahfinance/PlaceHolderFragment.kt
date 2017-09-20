fe8dcf0f3ebb3f5b514aefe11864db271cc6af95
deniprayoga <deniprayogaa@gmail.com>
2017-09-21 06:08:57 +0700

    restructure code

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/PlaceHolderFragment.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/PlaceHolderFragment.kt
@@ -7,30 +7,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_main.view.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
- * Created by denip on 8/17/2017.
+ * Created by denip on 9/15/2017.
  */
 
-class PlaceholderFragment : Fragment(), View.OnClickListener {
+class PlaceHolderFragment : Fragment(), View.OnClickListener {
+
+    companion object {
+
+        private val ARG_SECTION_NUMBER = "section_number"
+
+        fun newInstance(sectionNumber: Int): PlaceHolderFragment {
+            val fragment = PlaceHolderFragment()
+            val args = Bundle()
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+            fragment.arguments = args
+            return fragment
+        }
+    }
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        if (arguments.getInt(ARG_SECTION_NUMBER) == 0) {
-            inflateExpenseFragment(inflater)
-        } else if (arguments.getInt(ARG_SECTION_NUMBER) == 1) {
-            val fragmentMainView = inflater!!.inflate(R.layout.fragment_main, container, false)
-            fragmentMainView?.setting_button?.setOnClickListener(this)
-            return fragmentMainView
-        } else if (arguments.getInt(ARG_SECTION_NUMBER) == 2) {
-            inflateIncomeFragment(inflater)
+        val fragmentMainView = inflater!!.inflate(R.layout.fragment_main, container, false)
+        when {
+            arguments.getInt(ARG_SECTION_NUMBER) == 0 -> inflateExpenseFragment(inflater)
+            arguments.getInt(ARG_SECTION_NUMBER) == 1 -> {
+                fragmentMainView?.setting_button?.setOnClickListener(this)
+                return fragmentMainView
+            }
+            arguments.getInt(ARG_SECTION_NUMBER) == 2 -> inflateIncomeFragment(inflater)
         }
+
+        initLayout()
+        initAuth()
         return null
+    }
+
+    private fun initLayout() {
+
     }
 
     override fun onClick(v: View?) {
@@ -45,16 +65,20 @@ class PlaceholderFragment : Fragment(), View.OnClickListener {
     private fun inflateIncomeFragment(inflater: LayoutInflater?): View? =
         inflater?.inflate(R.layout.fragment_income, container, false)
 
-    companion object {
+    private fun launchLoginActivity() {
+        startActivity(Intent(context, LoginActivity::class.java))
+    }
 
-        private val ARG_SECTION_NUMBER = "section_number"
+    private fun initAuth() {
+        val authListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user: FirebaseUser? = firebaseAuth.currentUser
 
-        fun newInstance(sectionNumber: Int): PlaceholderFragment {
-            val fragment = PlaceholderFragment()
-            val args = Bundle()
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-            fragment.arguments = args
-            return fragment
+            if (user == null) {
+                activity.supportFragmentManager.beginTransaction().remove(this).commit()
+                launchLoginActivity()
+            }
         }
+
+        auth.addAuthStateListener(authListener)
     }
 }
