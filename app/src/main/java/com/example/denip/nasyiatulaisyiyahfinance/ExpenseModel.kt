0aeee91917c4e72016bc887d8665f8ae60159624
deniprayoga <deniprayogaa@gmail.com>
2017-09-21 16:18:46 +0700

    rename file from Expense to ExpenseModel

---
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/ExpenseModel.kt
@@ -0,0 +1,15 @@
+package com.example.denip.nasyiatulaisyiyahfinance
+
+/**
+ * Created by denip on 9/18/2017.
+ */
+
+data class ExpenseModel(val addedByTreasure: String?,
+                        val amount: Int?,
+                        val category: String?,
+                        val dateCreated: String?,
+                        val expenseId: String?,
+                        val note: String?,
+                        val notePhotoUri: String?) {
+    constructor() : this("", 0, "", "", "", "", "")
+}
