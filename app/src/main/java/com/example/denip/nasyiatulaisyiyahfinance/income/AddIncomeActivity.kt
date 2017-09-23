3545419b6c0fa33810f99291d88e15b7db7bfeb7
deniprayoga <deniprayogaa@gmail.com>
2017-09-23 09:01:11 +0700

    refactor AddIncomeActivity

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/income/AddIncomeActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/income/AddIncomeActivity.kt
@@ -1,4 +1,4 @@
-package com.example.denip.nasyiatulaisyiyahfinance
+package com.example.denip.nasyiatulaisyiyahfinance.income
 
 import android.Manifest
 import android.content.Intent
@@ -14,6 +14,7 @@ import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment
+import com.example.denip.nasyiatulaisyiyahfinance.R
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import gun0912.tedbottompicker.TedBottomPicker
