3d12dfeecef25a68817cc392e175f30711b45d87
deniprayoga <deniprayogaa@gmail.com>
2017-09-27 09:56:47 +0700

    refactor CategoryExpenseSetting class`

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/category/CategoryExpenseSettingActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/category/CategoryExpenseSettingActivity.kt
@@ -1,4 +1,4 @@
-package com.example.denip.nasyiatulaisyiyahfinance.category
+package com.example.denip.nasyiatulaisyiyahfinance.expense.category
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
