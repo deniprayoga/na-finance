00ab54e1f3b8c29c10397309840d6d800cf8d3eb
deniprayoga <deniprayogaa@gmail.com>
2017-09-28 13:48:37 +0700

    - create CategoryIncomeSettingActivity class - able to create income category

---
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/income/category/CategoryIncomeSettingActivity.kt
@@ -0,0 +1,125 @@
+package com.example.denip.nasyiatulaisyiyahfinance.income.category
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.denip.nasyiatulaisyiyahfinance.R
+import com.example.denip.nasyiatulaisyiyahfinance.expense.category.CategoryModel
+import com.example.denip.nasyiatulaisyiyahfinance.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_add_income_category.view.*
+import kotlinx.android.synthetic.main.activity_category_income_setting.*
+
+class CategoryIncomeSettingActivity : AppCompatActivity() {
+
+    companion object {
+        private val auth = FirebaseAuth.getInstance()
+        private val dbCategoryRef = FirebaseDatabase.getInstance().getReference("categories/income")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_income_setting)
+
+        initLayout()
+        initAuth()
+    }
+
+    private fun initAuth() {
+        val authListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user: FirebaseUser? = firebaseAuth.currentUser
+
+            if (user == null) {
+                finish()
+                launchLoginActivity()
+            }
+        }
+        auth.addAuthStateListener(authListener)
+    }
+
+    private fun launchLoginActivity() {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    private fun initLayout() {
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar_category_income_setting_layout)
+
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_category_setting, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> onBackPressed()
+            R.id.action_bar_add_category -> showAddExpenseCategoryDialog()
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAddExpenseCategoryDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        val categoryInflater = layoutInflater
+        val view = categoryInflater.inflate(R.layout.activity_add_income_category, null)
+
+        dialogBuilder
+            .setView(view)
+            .setTitle(getString(R.string.add_category))
+
+        val dialog = dialogBuilder.create()
+        dialog.show()
+
+        view?.button_add_category_income?.setOnClickListener {
+            when {
+                view.add_category_number_first?.text!!.isEmpty() ->
+                    view.add_category_number_first?.error = getString(R.string.prompt_empty_field)
+                view.add_category_number_first.text.toString().toInt() == 0 ->
+                    view.add_category_number_first?.error = getString(R.string.first_number_zero_error)
+                view.add_category_number_second.text.isEmpty() ->
+                    view.add_category_number_second?.error = getString(R.string.prompt_empty_field)
+                view.add_category_number_third.text.isEmpty() ->
+                    view.add_category_number_third?.error = getString(R.string.prompt_empty_field)
+                view.add_category_name_field.text.isEmpty() ->
+                    view.add_category_name_field?.error = getString(R.string.prompt_empty_field)
+                else -> {
+                    val firstNumber = view.add_category_number_first?.text.toString()
+                    val secondNumber = view.add_category_number_second?.text.toString().toInt()
+                    val thirdNumber = view.add_category_number_third?.text.toString().toInt()
+                    val categoryNumber = firstNumber.toString() + "-" + secondNumber.toString() +
+                        "-" + thirdNumber.toString()
+                    val categoryName = view.add_category_name_field?.text.toString()
+
+                    Log.d("hunter_ini", categoryNumber)
+                    Log.d("hunter_categoryName", categoryName)
+                    Log.d("hunter_firstNumber", firstNumber.toString())
+                    Log.d("hunter_secondNumber", secondNumber.toString())
+                    Log.d("hunter_thirdNumber", thirdNumber.toString())
+
+                    val categoryId = dbCategoryRef?.push()?.key
+                    val category = CategoryModel(categoryId, firstNumber.toInt(), secondNumber,
+                        thirdNumber, categoryNumber, categoryName)
+                    dbCategoryRef?.child(categoryId)?.setValue(category)
+                    Toast.makeText(this, getString(R.string.category_added), Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+            }
+        }
+
+    }
+}
