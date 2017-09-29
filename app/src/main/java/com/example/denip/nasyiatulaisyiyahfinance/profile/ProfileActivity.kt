709334f654af595768742082d172c06a0609970f
deniprayoga <deniprayogaa@gmail.com>
2017-09-29 10:16:50 +0700

    - able to read profile data from database - able to update profile data from database

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/profile/ProfileActivity.kt
@@ -4,15 +4,22 @@ import android.Manifest
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.example.denip.nasyiatulaisyiyahfinance.R
+import com.example.denip.nasyiatulaisyiyahfinance.user.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import gun0912.tedbottompicker.TedBottomPicker
@@ -22,6 +29,8 @@ import java.util.ArrayList
 class ProfileActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
+        private val dbProfile = FirebaseDatabase.getInstance().getReference("users")
+        private val auth = FirebaseAuth.getInstance()
         var selectedUri: Uri? = null
         lateinit var glideRequestManager: RequestManager
     }
@@ -36,36 +45,35 @@ class ProfileActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun initLayout() {
         initToolbar()
-        initLocations()
         profile_photo.setOnClickListener(this)
-        profile_photo.isClickable = false
-    }
-
-    private fun initLocations() {
-        val locationAdapter: ArrayAdapter<String> = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, resources.getStringArray(R.array.locations))
-        locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        location_spinner.adapter = locationAdapter
+        location_profile_field.setOnClickListener(this)
     }
 
     private fun initToolbar() {
         setSupportActionBar(toolbar_profile_layout)
-
-        edit_profile_button.setOnClickListener(this)
-        save_profile_button.setOnClickListener(this)
         supportActionBar!!.setHomeButtonEnabled(true)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.edit_profile_button -> enableFields()
-            R.id.save_profile_button -> {
-                disableFields()
-                Toast.makeText(this, "Make your save profile action", Toast.LENGTH_SHORT).show()
-            }
             R.id.profile_photo -> pickImage()
+            R.id.location_profile_field -> showLocationDialog()
         }
+    }
+
+    private fun showLocationDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder
+            .setTitle(getString(R.string.location))
+            .setNeutralButton(getString(R.string.jakarta), { dialog, which ->
+                location_profile_field?.setText(getString(R.string.jakarta))
+            })
+            .setPositiveButton(getString(R.string.yogyakarta), { dialog, which ->
+                location_profile_field?.setText(getString(R.string.yogyakarta))
+            })
+        val dialog = dialogBuilder.create()
+        dialog.show()
     }
 
     private fun pickImage() {
@@ -101,7 +109,8 @@ class ProfileActivity : AppCompatActivity(), View.OnClickListener {
 
         TedPermission(this@ProfileActivity)
             .setPermissionListener(permissionListener)
-            .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+            .setDeniedMessage("If you reject permission,you can not use this service" +
+                "\n\nPlease turn on permissions at [Setting] > [Permission]")
             .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             .check()
     }
@@ -111,30 +120,88 @@ class ProfileActivity : AppCompatActivity(), View.OnClickListener {
             deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
     }
 
-    private fun enableFields() {
-        profile_name_field.isEnabled = true
-        phone_number_profile_field.isEnabled = true
-        location_spinner.isClickable = true
-        profile_photo.isClickable = true
-        edit_profile_button.visibility = View.INVISIBLE
-        save_profile_button.visibility = View.VISIBLE
-    }
-
-    private fun disableFields() {
-        profile_name_field.isEnabled = false
-        phone_number_profile_field.isEnabled = false
-        location_spinner.isClickable = false
-        profile_photo.isClickable = false
-        edit_profile_button.visibility = View.VISIBLE
-        save_profile_button.visibility = View.INVISIBLE
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_profile, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
         when (id) {
             android.R.id.home -> onBackPressed()
+            R.id.action_bar_save -> {
+                var fullName = profile_name_field?.text.toString()
+                var phoneNumber = phone_number_profile_field?.text.toString()
+
+                when {
+                    fullName.isEmpty() -> profile_name_field?.error = getString(R.string.prompt_profile_name_empty)
+                    phoneNumber.isEmpty() -> phone_number_profile_field?.error =
+                        getString(R.string.prompt_profile_phone_number_empty)
+                    else -> {
+                        val location = location_profile_field?.text.toString()
+                        val userId = auth.currentUser?.uid
+                        val userEmail = auth.currentUser?.email
+                        fullName = profile_name_field?.text.toString()
+                        phoneNumber = phone_number_profile_field?.text.toString()
+
+                        val profileData = UserModel(userId, fullName, phoneNumber, location, userEmail, null)
+                        updateProfile(profileData)
+                    }
+                }
+            }
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateProfile(profileData: UserModel?) {
+        val dbUpdateProfileRef = dbProfile.child(profileData?.userId)
+        dbUpdateProfileRef.setValue(profileData)
+        Toast.makeText(this, getString(R.string.profile_updated), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("taggg", "In the onStart() event")
+
+        dbProfile.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError?) {
+
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot?) {
+                val fullName = dataSnapshot?.child(auth.currentUser?.uid)?.child("fullName")?.value.toString()
+                val location = dataSnapshot?.child(auth.currentUser?.uid)?.child("location")?.value.toString()
+                val phoneNumber = dataSnapshot?.child(auth.currentUser?.uid)?.child("phoneNumber")?.value.toString()
+                profile_name_field.setText(fullName)
+                phone_number_profile_field.setText(phoneNumber)
+                location_profile_field.setText(location)
+            }
+        })
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("taggg", "In the onRestart() event")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("taggg", "In the onResume() event")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("taggg", "In the onPause() event")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("taggg", "In the onStop() event")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("taggg", "In the onDestroy() event")
     }
 }
