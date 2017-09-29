e77abb7f1bf5c2d1a3d5070320e49226b70809fc
deniprayoga <deniprayogaa@gmail.com>
2017-09-29 10:22:37 +0700

    - add empty constructor on UserModel data class - change some variable name

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/user/UserModel.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/user/UserModel.kt
@@ -4,9 +4,11 @@ package com.example.denip.nasyiatulaisyiyahfinance.user
  * Created by denip on 9/17/2017.
  */
 
-data class UserModel(val id: String?,
-                     val name: String?,
+data class UserModel(val userId: String?,
+                     val fullName: String?,
                      val phoneNumber: String?,
                      val location: String?,
                      val email: String?,
-                     val photoUri: String?)
+                     val photoUri: String?) {
+    constructor() : this("", "", "", "", "", "")
+}
