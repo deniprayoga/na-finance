6042da8e71ffc9c3f9367c9356f69348c455049e
deniprayoga <deniprayogaa@gmail.com>
2017-10-18 12:39:02 +0700

    update ExpenseModel class

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/ExpenseModel.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/ExpenseModel.kt
@@ -4,12 +4,13 @@ package com.example.denip.nasyiatulaisyiyahfinance.expense
  * Created by denip on 9/18/2017.
  */
 
-data class ExpenseModel(val addedByTreasure: String?,
-                        val amount: Int?,
-                        val category: String?,
-                        val dateCreated: String?,
-                        val expenseId: String?,
-                        val note: String?,
-                        val notePhotoUri: String?) {
-    constructor() : this("", 0, "", "", "", "", "")
+class ExpenseModel(val addedByTreasure: String?,
+                   val amount: Int?,
+                   val category: String?,
+                   val dateCreated: String?,
+                   val expenseId: String?,
+                   val note: String?,
+                   val notePhotoUri: String?,
+                   val categoryId: String?) {
+    constructor() : this("", 0, "", "", "", "", "", "")
 }
