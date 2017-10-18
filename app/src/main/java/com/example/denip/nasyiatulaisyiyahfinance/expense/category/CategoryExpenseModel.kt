dcb731d49f4f760e3745533c55ec4fb8909657d5
deniprayoga <deniprayogaa@gmail.com>
2017-10-18 11:14:39 +0700

    updatee CategoryExpenseModel

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/category/CategoryExpenseModel.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/category/CategoryExpenseModel.kt
@@ -4,11 +4,11 @@ package com.example.denip.nasyiatulaisyiyahfinance.expense.category
  * Created by denip on 9/19/2017.
  */
 
-data class CategoryExpenseModel(val categoryId: String?,
-                                val firstNumber: Int?,
-                                val secondNumber: Int?,
-                                val thirdNumber: Int?,
-                                val categoryNumber: String?,
-                                val categoryName: String?) {
+class CategoryExpenseModel(var categoryId: String?,
+                           var firstNumber: Int?,
+                           var secondNumber: Int?,
+                           var thirdNumber: Int?,
+                           var categoryNumber: String?,
+                           var categoryName: String?) {
     constructor() : this("", 0, 0, 0, "", "")
 }
