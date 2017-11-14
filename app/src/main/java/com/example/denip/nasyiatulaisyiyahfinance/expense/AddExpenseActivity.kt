2e9b8aa1d5d768441e56af408a4fadc23b980a7b
deniprayoga <deniprayogaa@gmail.com>
2017-11-14 14:40:55 +0700

    - remove unnecessary variables and functions - change error warning with shake animation

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/AddExpenseActivity.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/AddExpenseActivity.kt
@@ -1,9 +1,7 @@
 package com.example.denip.nasyiatulaisyiyahfinance.expense
 
-import android.Manifest
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
@@ -13,7 +11,7 @@ import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.LinearLayout
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
@@ -27,9 +25,6 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.gun0912.tedpermission.PermissionListener
-import com.gun0912.tedpermission.TedPermission
-import gun0912.tedbottompicker.TedBottomPicker
 import kotlinx.android.synthetic.main.activity_add_amount_expense.view.*
 import kotlinx.android.synthetic.main.activity_add_expense.*
 import org.joda.time.DateTime
@@ -42,7 +37,6 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
     companion object {
         private val HUNTR = "huntr_AddExpense"
         private val FRAG_TAG_DATE_PICKER = "fragment_date_picker_name"
-        private var selectedUri: Uri? = null
         private lateinit var glideRequestManager: RequestManager
         private val auth = FirebaseAuth.getInstance()
         private val uid = auth.currentUser?.uid
@@ -67,11 +61,7 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
         expense_categories_field.isFocusable = false
         initToolbar()
         showCurrentDate()
-        calendar_button_expense.setOnClickListener(this)
         calendar_result_text_expense.setOnClickListener(this)
-        pick_image_button_expense.setOnClickListener(this)
-        image_preview_expense.setOnClickListener(this)
-        layout_pick_image_expense.setOnClickListener(this)
         expense_amount_field.setOnClickListener(this)
         expense_categories_field.setOnClickListener(this)
         expense_note_field.setOnClickListener(this)
@@ -120,13 +110,22 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
         val dateCreated = calendar_result_text_expense?.text.toString()
         val amount = expense_amount_field?.text.toString()
         val note = expense_note_field?.text.toString()
-        val notePhotoUri = selectedUri.toString()
+        //val addedByTreasurerInitial = selectedUri.toString()
         val category = expense_categories_field?.text.toString()
 
         when {
-            amount.isEmpty() -> expense_amount_field.error = getString(R.string.prompt_amount_empty)
-            note.isEmpty() -> expense_note_field.error = getString(R.string.prompt_note_empty)
-            category.isEmpty() -> expense_categories_field.error = getString(R.string.prompt_category_empty)
+            amount.isEmpty() -> {
+                val shake = AnimationUtils.loadAnimation(this@AddExpenseActivity, R.anim.shake)
+                expense_amount_field.startAnimation(shake)
+            }
+            note.isEmpty() -> {
+                val shake = AnimationUtils.loadAnimation(this@AddExpenseActivity, R.anim.shake)
+                expense_note_field.startAnimation(shake)
+            }
+            category.isEmpty() -> {
+                val shake = AnimationUtils.loadAnimation(this@AddExpenseActivity, R.anim.shake)
+                expense_categories_field.startAnimation(shake)
+            }
             else -> {
 
                 val expenseId: String? = dbRef?.push()?.key
@@ -145,6 +144,10 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
                     override fun onDataChange(dataSnapshot: DataSnapshot?) {
                         val fullName = dataSnapshot?.child(uid)?.child("fullName")
                             ?.value.toString()
+
+                        val initial = dataSnapshot?.child(uid)?.child("initial")?.value.toString()
+
+                        Log.d(HUNTR, "initial : " + initial)
                         Log.d(HUNTR, "fullName : " + fullName)
 
                         val expense = ExpenseModel(
@@ -155,14 +158,15 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
                             dateCreated + "^",
                             expenseId,
                             note + "^",
-                            notePhotoUri,
+                            initial,
                             categoryId)
 
                         dbRef?.child("expenses")?.child(expenseId)?.setValue(expense)
 
                         Log.d(HUNTR, "expense amount : " + expense.amount)
                         Log.d(HUNTR, "expense note : " + expense.note)
-                        Log.d(HUNTR, "expense note photo uri : " + expense.notePhotoUri)
+                        Log.d(HUNTR, "expense added by treasurer uid : " + expense.addedByTreasurerUid)
+                        Log.d(HUNTR, "expense added by treasurer initial : " + expense.addedByTreasurerInitial)
                         Log.d(HUNTR, "expense added by treasurer : " + expense.addedByTreasurer)
                         Log.d(HUNTR, "expense category : " + expense.category)
                         Log.d(HUNTR, "expense category id : " + expense.categoryId)
@@ -266,13 +270,10 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.calendar_button_expense -> showCalendar()
-            R.id.pick_image_button_expense -> pickImage()
             R.id.expense_amount_field -> showAddAmountDialog()
-            R.id.calendar_result_text_expense -> showCalendar()
+            R.id.calendar_result_text_income -> showCalendar()
             R.id.expense_categories_field -> pickCategory()
             R.id.expense_note_field -> showCursorOnNoteField()
-            R.id.image_preview_expense -> pickImage()
-            R.id.layout_pick_image_expense -> pickImage()
         }
     }
 
@@ -288,51 +289,6 @@ class AddExpenseActivity : AppCompatActivity(), View.OnClickListener,
 
     private fun hideCursorOnNoteField() {
         expense_note_field.isCursorVisible = false
-    }
-
-    private fun pickImage() {
-        val permissionListener: PermissionListener = object : PermissionListener {
-            override fun onPermissionGranted() {
-                val bottomSheetDialogFragment: TedBottomPicker = TedBottomPicker.Builder(this@AddExpenseActivity)
-                    .setOnImageSelectedListener { uri ->
-                        Log.d("ted", "uri:" + uri)
-                        Log.d("ted", "uri.path:" + uri.path)
-                        selectedUri = uri
-
-                        image_preview_expense.visibility = View.VISIBLE
-                        image_preview_expense.post {
-                            glideRequestManager
-                                .load(uri)
-                                .fitCenter()
-                                .into(image_preview_expense)
-                        }
-
-                        image_preview_expense.layoutParams = LinearLayout.LayoutParams(800, 800)
-                        hint_pick_image?.visibility = View.GONE
-                    }
-
-                    .setSelectedUri(selectedUri)
-                    .setPeekHeight(1200)
-                    .create()
-                bottomSheetDialogFragment.show(supportFragmentManager)
-            }
-
-            override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
-                showPermissionDenied(deniedPermissions)
-            }
-        }
-
-        TedPermission(this@AddExpenseActivity)
-            .setPermissionListener(permissionListener)
-            .setDeniedMessage(getString(R.string.ted_permission_denied))
-            .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            .check()
-        hideCursorOnNoteField()
-    }
-
-    private fun showPermissionDenied(deniedPermissions: ArrayList<String>?) {
-        Toast.makeText(this@AddExpenseActivity, "Permission denied\n" +
-            deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
     }
 
     private fun launchLoginActivity() {
