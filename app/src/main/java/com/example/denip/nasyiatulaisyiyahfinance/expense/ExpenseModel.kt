9db17096d451d97e4bdbab84e2c4ddc24458b109
deniprayoga <deniprayogaa@gmail.com>
2017-11-15 13:00:26 +0700

    rename variables on expense model

--- app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/ExpenseModel.kt
+++ app/src/main/java/com/example/denip/nasyiatulaisyiyahfinance/expense/ExpenseModel.kt
@@ -5,13 +5,13 @@ package com.example.denip.nasyiatulaisyiyahfinance.expense
  */
 
 class ExpenseModel(val addedByTreasurer: String?,
-                   val treasurerUid: String?,
+                   val addedByTreasurerUid: String?,
                    val amount: String?,
                    val category: String?,
                    val dateCreated: String?,
                    val expenseId: String?,
                    val note: String?,
-                   val notePhotoUri: String?,
+                   val addedByTreasurerInitial: String?,
                    val categoryId: String?) {
     constructor() : this("","", "", "", "", "", "", "", "")
     constructor(addedByTreasurer: String?) : this(addedByTreasurer, "", "", "", "", "", "", "", "")
